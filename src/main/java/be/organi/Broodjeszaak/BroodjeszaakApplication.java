5bc7b432dc10efa43fc52fc9c72870e7a594f0c3
SamDC97 <sam.decock97@gmail.com>
2021-02-22 17:04:31 +0100

    initial commit

---
+++ src/main/java/be/organi/Broodjeszaak/BroodjeszaakApplication.java
@@ -0,0 +1,13 @@
+package be.organi.Broodjeszaak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroodjeszaakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroodjeszaakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/organi/Broodjeszaak/BroodjeszaakApplicationTests.java
@@ -0,0 +1,13 @@
+package be.organi.Broodjeszaak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroodjeszaakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
